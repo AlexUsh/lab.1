4597bf53f0b516f2d5053524227987c05bff8e2e
AlexUsh <31797507+AlexUsh@users.noreply.github.com>
2017-10-23 20:07:48 +0300

    Update lab.1.c

--- lab.1.c
+++ lab.1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 #include <string.h>
 int main(int argc, char *argv[])
 {
@@ -54,7 +53,6 @@ int main(int argc, char *argv[])
       printf ("%.2g K: \n", z);
       printf ("%.2f C\n", c);
       printf ("%.2f F\n", f);
-    }
-    getch ();
+    }    
     return 0;
 }
