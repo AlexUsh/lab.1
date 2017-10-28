feef1cacd5e8b95e107827779a67833bf1322ba4
AlexUsh <31797507+AlexUsh@users.noreply.github.com>
2017-10-28 19:10:05 +0300

    Update lab.1.c

--- lab.1.c
+++ lab.1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 #include <string.h>
 int main(int argc, char *argv[])
 {
@@ -10,49 +11,97 @@ int main(int argc, char *argv[])
     {
         if (strcmp(argv[2],"C")==0)
         {
-          k=z+273.15;
-          f=9*(z+32)/9;
-          printf ("%.2f C\n", z);
-          printf ("%.2f K\n", k);
-          printf ("%.2f F\n", f);
+            k=z+273.15;
+            f=9*(z+32)/9;
+            if (k<0)
+            {
+                printf("menshe absolutnogo 0");
+            }
+            else
+            {
+                printf ("%.2f C\n", z);
+                printf ("%.2f K\n", k);
+                printf ("%.2f F\n", f);
+            }
         }
         else if (strcmp(argv[2],"F")==0)
         {
           c=5*(z-32)/9;
           k=5*(z-32)/9+273.15;
-          printf ("%.2f F\n", z);
-          printf("%.2f C\n", c);
-          printf("%.2f K\n", k);
+          if (k<0)
+            {
+                printf("menshe absolutnogo 0");
+            }
+            else
+            {
+                printf ("%.2f F\n", z);
+                printf("%.2f C\n", c);
+                printf("%.2f K\n", k);
+            }
 
         }
         else
         {
           c=z-273.15;
           f=9*(z-273.15)/5+32;
-          printf ("%.2f K\n", z);
-          printf ("%.2f C\n", c);
-          printf ("%.2f F\n", f);
+          if (z<0)
+            {
+                printf("menshe absolutnogo 0");
+            }
+            else
+            {
+                printf ("%.2f K\n", z);
+                printf ("%.2f C\n", c);
+                printf ("%.2f F\n", f);
+            }
         }
     }
     else
     {
-      k=z+273.15;
-      f=9*(z+32)/9;
-      printf ("%.2g C: \n", z);
-      printf ("%.2f K\n", k);
-      printf ("%.2f F\n\n", f);
 
-      c=5*(z-32)/9;
-      k=5*(z-32)/9+273.15;
-      printf ("%.2g F: \n", z);
-      printf("%.2f C\n", c);
-      printf("%.2f K\n\n", k);
+        k=z+273.15;
+        f=9*(z+32)/9;
+        if (k<0)
+        {
+            printf ("menshe absolutnogo 0 \n\n");
+        }
+        else
+        {
 
-      c=z-273.15;
-      f=9*(z-273.15)/5+32;
-      printf ("%.2g K: \n", z);
-      printf ("%.2f C\n", c);
-      printf ("%.2f F\n", f);
-    }    
+            printf ("%.2f C: \n", z);
+            printf ("%.2f K\n", k);
+            printf ("%.2f F\n\n", f);
+        }
+
+
+        c=5*(z-32)/9;
+        k=5*(z-32)/9+273.15;
+        if (k<0)
+        {
+            printf("menshe absolutnogo 0 \n\n");
+        }
+        else
+        {
+            printf ("%.2f F: \n", z);
+            printf("%.2f C\n", c);
+            printf("%.2f K\n\n", k);
+        }
+
+
+        c=z-273.15;
+        f=9*(z-273.15)/5+32;
+         if (z<0)
+        {
+            printf("menshe absolutnogo 0 \n\n");
+        }
+        else
+        {
+            printf ("%.2f K: \n", z);
+            printf ("%.2f C\n", c);
+            printf ("%.2f F\n", f);
+        }
+    }
+
+    getch ();
     return 0;
 }
